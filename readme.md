e417080fdb771b9bd20918b9e118601bb4f7cf7e
Sourabh Rustagi <sourabh.rustagi@hotmail.com>
2022-03-25 01:28:43 +0530

    platform mode to tf

--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
+# Evolution 
+
+Could-first
 
 # Docker
 
--- tf/deployment.tf
+++ tf/deployment.tf
@@ -1,11 +1,14 @@
-resource "kubernetes_deployment" "helloworld" {
+resource "kubernetes_deployment" "platformservice" {
   metadata {
     name = "platforms-tf-depl"
+    labels = {
+      app = "platformservice"
+    }
   }
   spec {
     selector {
       match_labels = {
-        "app" = "platformservice"
+        app = "platformservice"
       }
     }
     replicas = 2
@@ -13,7 +16,7 @@ resource "kubernetes_deployment" "helloworld" {
 
       metadata {
         labels = {
-          "app" = "platformservice"
+          app = "platformservice"
         }
       }
 
@@ -28,5 +31,43 @@ resource "kubernetes_deployment" "helloworld" {
       }
 
     }
+  }
+}
+
+resource "kubernetes_service" "example" {
+  metadata {
+    name = "platform-cluster-ip"
+  }
+  spec {
+    selector = {
+      app = kubernetes_deployment.platformservice.metadata.0.labels.app
+    }
+    port {
+      name = kubernetes_deployment.platformservice.metadata.0.labels.app
+      port        = 80
+      target_port = 80
+      protocol = "TCP"
+    }
+
+    type = "ClusterIP"
+  }
+}
+
+resource "kubernetes_service" "platform-node-port" {
+  metadata {
+    name = "platformservice-srv"
+  }
+  spec {
+    selector = {
+      app = kubernetes_deployment.platformservice.metadata.0.labels.app
+    }
+    port {
+      name = kubernetes_deployment.platformservice.metadata.0.labels.app
+      port        = 80
+      target_port = 80
+      protocol = "TCP"
+    }
+
+    type = "NodePort"
   }
 }
